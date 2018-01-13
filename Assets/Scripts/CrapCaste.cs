aea7076cbccbe4a8fd6d3443987bc0cda5f8286d
keilaloia <keilaloia@gmail.com>
2018-01-13 13:13:38 -0800

    progress

--- Assets/Scripts/CrapCaste.cs
+++ Assets/Scripts/CrapCaste.cs
@@ -5,41 +5,85 @@ using UnityEngine;
 public class CrapCaste : MonoBehaviour
 {
 
-	private Rigidbody rb;
+	private Rigidbody _rb;
+	private RaycastHit _hit;
+	private CameraSingleTon _cams;
+    private AnchorSingleton _PivotPoint;
+	private Transform _me;
+	private Vector3 _fDist;
+    private float _RotateSpeed = 1;
+    public float length;
+    public Vector3 offset;
+    //couldnt get to work with singlton
+    public Transform pPoint;
 
-	private RaycastHit hit;
 
-	public float length;
-	public Vector3 offset;
-	private CameraSingleTon cams;
-
-	private Vector3 fDist;
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
-	   // offset = new Vector3(0, 1.5f, -5.0f);
-		rb = GetComponent<Rigidbody>();
-		cams = CameraSingleTon.instance.GetComponent<CameraSingleTon>();
+        GetCompInit();
+
+        pPoint.transform.position = _me.transform.position;
+        pPoint.transform.parent = _me.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate ()
 	{
-		caste();
-		cams.transform.position = fDist;
-		Debug.DrawLine(transform.position, transform.position + offset, Color.blue);
+        CalcfDist();
+        cameraRotation();
 	}
 
-	void caste()
+    void GetCompInit()
+    {
+        // offset = new Vector3(0, 1.5f, -5.0f);
+        _rb = GetComponent<Rigidbody>();
+        _cams = CameraSingleTon.instance.GetComponent<CameraSingleTon>();
+        _me = GetComponent<Transform>();
+        _PivotPoint = GetComponent<AnchorSingleton>();
+    }
+    void CalcfDist()
+    {
+        caste();
+        _cams.transform.position = _fDist;
+        Debug.DrawLine(transform.position, transform.position + offset, Color.blue);
+
+    }
+    void caste()
 	{
-		if(Physics.Linecast(transform.position, transform.position + offset, out hit))
+		if(Physics.Linecast(transform.position, transform.position + offset, out _hit))
 		{
-			fDist = transform.position + offset;
+			_fDist = transform.position + offset;
 
 		}
 		else
 		{
-			fDist = transform.position + offset;
+			_fDist = transform.position + offset;
 		}
 	}
+
+    void cameraRotation()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * _RotateSpeed;
+        _me.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * _RotateSpeed;
+        pPoint.transform.Rotate(-vertical, 0, 0);
+
+        //move camera based on the current rotation of the target & the original offset
+        float desiredYAngle = _me.eulerAngles.y;
+        float desiredXAngle = pPoint.transform.eulerAngles.x;
+        _cams.transform.rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        _cams.transform.position = _me.position - (_cams.transform.rotation * offset);
+
+        if(_cams.transform.position.y < _me.position.y)
+        {
+            _cams.transform.position = new Vector3(_cams.transform.position.x, _me.position.y -.5f, _cams.transform.position.z);
+        }
+
+        _cams.transform.LookAt(_me);
+
+    }
 }
---
+++ Assets/Scripts/CrapMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CrapMovement : MonoBehaviour
+{
+
+	private float _speed = 10f;
+    private float _jumpforce = 15f;
+    private CharacterController _Pcharacter;
+    private Vector3 _mDir;
+    private float _gravityscale = 5f;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        _Pcharacter = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+	}
+
+    void Movement()
+    {
+        float yStore = _mDir.y;
+        _mDir = ((transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal")));
+        _mDir = _mDir.normalized * -_speed;
+        _mDir.y = yStore;
+        
+        if(_Pcharacter.isGrounded)
+        {
+            _mDir.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                _mDir.y = _jumpforce;
+            }
+        }
+
+        
+        
+
+        _mDir.y = (_mDir.y + (Physics.gravity.y * _gravityscale * Time.deltaTime));
+        _Pcharacter.Move(_mDir * Time.deltaTime);
+    }
+}
