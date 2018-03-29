19587ea4baf61f15c5023e6020b4e364ae4d7033
Joshua Livingston <s179132@students.aie.edu.au>
2018-03-29 15:46:41 -0700

    K man did clime

---
+++ Assets/Scripts/AddScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour {
+
+    public GameObject Coin;
+    int point;
+    Score score;
+
+	void Start ()
+    {
+        point = 1;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Score.score += point;
+
+        DestroyObject(Coin);
+    }
+
+
+    void FixedUpdate ()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 80); 
+	}
+}
