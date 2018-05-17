611565d8249cff1b3b38d55688c3c9b11311fe74
Joshua Livingston <s179132@students.aie.edu.au>
2018-05-17 15:05:38 -0700

    merge with AI

---
+++ Assets/Scripts/Healone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healone : MonoBehaviour
+{
+    private PlayerStats statsinstance;
+
+
+    private void Start()
+    {
+        statsinstance = PlayerStats.instance.GetComponent<PlayerStats>();
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        //Debug.Log("derp");
+        if (other.gameObject.layer == 10)
+        {
+
+            statsinstance.GainHealth();
+            Destroy(gameObject);
+
+        }
+
+    }
+}
