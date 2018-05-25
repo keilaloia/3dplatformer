8b515b96ce900fe31a0285edc5bea55f5656d359
keilaloia <keil.aloia@students.aie.edu.au>
2018-05-24 17:56:50 -0700

    just add image

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+    public Text NameText;
+    public Text dialoguetext;
+
+    private Queue<string> Sentences;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Sentences = new Queue<string>();
+	}
+	
+    public void StartDialogue(Dial dialogue)
+    {
+        NameText.text = dialogue.name;
+        Sentences.Clear();
+
+        foreach(string Sentence  in dialogue.sentences)
+        {
+            Sentences.Enqueue(Sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(Sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = Sentences.Dequeue();
+        dialoguetext.text = sentence;
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("end of convo");
+    }
+}
---
+++ Assets/FinalGame/Keil/InteractDialgue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractDialgue : MonoBehaviour
+{
+    public Dial dialz;
+
+    public void TriggerDial()
+    {
+        FindObjectOfType<Dialogue>().StartDialogue(dialz);
+    }
+}
