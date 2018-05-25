64e3c4b36963f35ecae44ebb785a214d64603b63
keilaloia <keil.aloia@students.aie.edu.au>
2018-05-25 11:28:28 -0700

    FinalBuild

---
+++ Assets/FinalGame/Keil/Times.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Times : MonoBehaviour {
+
+
+    public float Timer;
+    private float StartTime;
+
+    public GameObject deletethis;
+    public GameObject deletethisToo;
+
+
+    private void Start()
+    {
+        StartTime = Timer;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        Timer -= Time.deltaTime;
+
+        if(Timer <= 0)
+        {
+            Timer = StartTime;
+            this.gameObject.SetActive(false);
+            deletethis.SetActive(false);
+            deletethisToo.SetActive(false);
+
+        }
+    }
+}
