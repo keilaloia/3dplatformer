f6c434f9f903eecc115f280387f630d09d86a056
keilaloia <keil.aloia@students.aie.edu.au>
2018-05-31 17:11:52 -0700

    better chatbox

--- Assets/FinalGame/Keil/Times.cs
+++ Assets/FinalGame/Keil/Times.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Times : MonoBehaviour {
 
@@ -8,26 +9,53 @@ public class Times : MonoBehaviour {
     public float Timer;
     private float StartTime;
 
-    public GameObject deletethis;
-    public GameObject deletethisToo;
+    [SerializeField]
+    private Text myTextelement;
+    public GameObject disable;
+    public string[] InputText;
 
+
+    private int i = 0;
+    private bool TimeTick = false;
 
     private void Start()
     {
         StartTime = Timer;
+        myTextelement.text = InputText[0];
+
+
     }
     // Update is called once per frame
-    void Update ()
+    private void Update ()
     {
         Timer -= Time.deltaTime;
 
-        if(Timer <= 0)
+        bool shouldSkip = Input.GetKeyUp(KeyCode.L) || Timer <= 0;
+        if (shouldSkip)
         {
+            TextDial();
             Timer = StartTime;
-            this.gameObject.SetActive(false);
-            deletethis.SetActive(false);
-            deletethisToo.SetActive(false);
-
+            TimeTick = false;
         }
     }
+
+
+
+    public void TextDial()
+    {
+        i++;
+
+        if (InputText.Length <= i)
+        {
+            disable.SetActive(false);
+            return;
+        }
+
+        myTextelement.text = InputText[i];
+    }
+
+
+
+
+
 }
